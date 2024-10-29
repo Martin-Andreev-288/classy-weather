ecfc94be69e76525f370e62ab8fb02082e3a34bb
Martin Andreev <martin_i_a@abv.bg>
2024-10-29 17:17:57 +0200

    Transform the input field into child component

--- src/App.js
+++ src/App.js
@@ -71,19 +71,17 @@ class App extends React.Component {
       this.setState({ isLoading: false });
     }
   };
+  // Showing the child to parent communication. The app is the same, so we're screenshotting only the code. In fact, this communication (like almost all things) are the same as in the functional components
+  setLocation = (e) => this.setState({ location: e.target.value });
 
   render() {
     return (
       <div className="app">
         <h1>Classy Weather</h1>
-        <div>
-          <input
-            type="text"
-            placeholder="Search for location..."
-            value={this.state.location}
-            onChange={(e) => this.setState({ location: e.target.value })}
-          />
-        </div>
+        <Input
+          location={this.state.location}
+          onChangeLocation={this.setLocation}
+        />
         <button onClick={this.fetchWeather}>Get weather</button>
 
         {this.state.isLoading && <p className="loader">Loading...</p>}
@@ -100,6 +98,21 @@ class App extends React.Component {
 }
 
 export default App;
+
+class Input extends React.Component {
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          placeholder="Search for location..."
+          value={this.props.location}
+          onChange={this.props.onChangeLocation}
+        />
+      </div>
+    );
+  }
+}
 
 class Weather extends React.Component {
   render() {
