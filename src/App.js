8da162ce0d096a320cb4ffe67f6fc10d308d9b48
Martin Andreev <martin_i_a@abv.bg>
2024-10-30 23:07:43 +0200

    Implement life cycle methods

--- src/App.js
+++ src/App.js
@@ -34,13 +34,15 @@ function formatDay(dateStr) {
 
 class App extends React.Component {
   state = {
-    location: "lisbon",
+    location: "",
     isLoading: false,
     displayLocation: "",
     weather: {},
   };
 
   fetchWeather = async () => {
+    if (this.state.location.length < 2) return this.setState({ weather: {} });
+
     try {
       this.setState({ isLoading: true });
 
@@ -66,13 +68,24 @@ class App extends React.Component {
       const weatherData = await weatherRes.json();
       this.setState({ weather: weatherData.daily });
     } catch (err) {
-      console.err(err);
+      console.error(err); // this was typed wrong (it was console.err)
     } finally {
       this.setState({ isLoading: false });
     }
   };
-  // Showing the child to parent communication. The app is the same, so we're screenshotting only the code. In fact, this communication (like almost all things) are the same as in the functional components
+
   setLocation = (e) => this.setState({ location: e.target.value });
+
+  componentDidMount() {
+    this.setState({ location: localStorage.getItem("location") || "" });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.location !== prevState.location) {
+      this.fetchWeather();
+      localStorage.setItem("location", this.state.location);
+    }
+  }
 
   render() {
     return (
@@ -82,7 +95,6 @@ class App extends React.Component {
           location={this.state.location}
           onChangeLocation={this.setLocation}
         />
-        <button onClick={this.fetchWeather}>Get weather</button>
 
         {this.state.isLoading && <p className="loader">Loading...</p>}
 
@@ -115,6 +127,10 @@ class Input extends React.Component {
 }
 
 class Weather extends React.Component {
+  componentWillUnmount() {
+    console.log("Weather will unmount");
+  }
+
   render() {
     const {
       temperature_2m_max: max,
